f2773aac5e81ceb3b9e19ce980ff049bddf6faf4
elius-w <hellius.af@gmail.com>
2021-12-07 17:27:34 -0300

    Instalação do projeto

---
+++ src/components/Section.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = () => {
+    return (
+        <wrap>
+            Section
+        </wrap>
+    )
+}
+
+export default Section
+
+const wrap = styled.div`
+
+`
