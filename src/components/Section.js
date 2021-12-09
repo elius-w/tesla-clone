1434bd4a3641fd148b3218487e8e62b66dcdef74
Helio Alves <hellius.af@gmail.com>
2021-12-08 22:10:40 -0300

    Menu finalizado e inclusão do redux

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 function Section ({title, description, leftBtnText, RightBtnText, backgroundImg }) {
 
@@ -7,25 +8,29 @@ function Section ({title, description, leftBtnText, RightBtnText, backgroundImg 
     return (
         <Wrap bgImage={backgroundImg}>
 
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
 
             <Buttons>
 
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
 
-                    {RightBtnText && 
-                     <RightButton>
-                        {RightBtnText}
-                     </RightButton>
-                    }
-                   
-                </ButtonGroup>
+                        {RightBtnText && 
+                        <RightButton>
+                            {RightBtnText}
+                        </RightButton>
+                        }
+                    
+                    </ButtonGroup>
+                </Fade>
 
                 <DownArrow src="/images/down-arrow.svg"/>
 
